10290f5c008c07d584fc35f61792a6ec8f582413
ruiz.terce <ruiz.terce@gmail.com>
2024-05-09 22:28:16 +0200

    Add error handling and revamp location search
    Errors during displayWeatherData() fetch now
    show a errorMessage in the DOM.
    
    Removed search button, now displayWeatherData()
    is called fired on locationElement's blur.

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -13,105 +13,111 @@ export default function initWeatherApp(containerElement) {
     locationElement.style.width = '70vh';
     locationElement.select();
   });
-  //Adjust locationElement width to value text length
+  //Refresh forecast info to the new location
   locationElement.addEventListener('blur', () => {
-    locationElement.style.width = eval(locationElement.value.length + 1) + 'ch';
-  });
-
-  const updateLocationBtn = document.createElement('button');
-  updateLocationBtn.className = 'button';
-  updateLocationBtn.textContent = 'Go';
-  containerElement.appendChild(updateLocationBtn);
-
-  updateLocationBtn.addEventListener('click', () => {
-    //Refresh forecast info to the new location
     displayWeatherData(containerElement, locationElement.value);
+    //Adjust locationElement width to value text length
+    locationElement.style.width = eval(locationElement.value.length + 1) + 'ch';
   });
 }
 
 //Add DOM elements inside a given containerElement, from the weatherObject returned by processWeatherData for a given location
 async function displayWeatherData(containerElement, location) {
-  if (document.querySelector('.forecast')) {
-    document.querySelector('.forecast').remove();
+  try {
+    //Remove prevous .forecast and .error
+    if (document.querySelector('.forecast')) {
+      document.querySelector('.forecast').remove();
+    }
+    if (document.querySelector('.error')) {
+      document.querySelector('.error').remove();
+    }
+
+    //Create forecast container element
+    const forecastElement = document.createElement('div');
+    forecastElement.className = 'forecast';
+    const weatherObject = await processWeatherData(location);
+    console.log('weatherObject');
+    console.log(weatherObject);
+
+    weatherObject.forecast.forecastday.forEach((dayObject) => {
+      //Create day element and header
+      const dayElement = document.createElement('div');
+      dayElement.classList.add('day');
+      const dateElement = document.createElement('h2');
+      dateElement.classList.add('date');
+      dateElement.textContent = dayObject.date;
+      dayElement.appendChild(dateElement);
+
+      const table = document.createElement('table');
+      table.classList.add('weather-table');
+
+      //Create table headers
+      const headerRow = document.createElement('tr');
+      const headers = ['Hour', 'Temperature', 'Humidity', 'Condition', 'Wind', 'Rain Chance'];
+      const headerClasses = ['hour', 'temp', 'humidity', 'condition', 'wind', 'rain-chance'];
+
+      headers.forEach((headerText, index) => {
+        const headerCell = document.createElement('th');
+        headerCell.textContent = headerText;
+        headerCell.classList.add(headerClasses[index]); // Add class to header cell
+        headerRow.appendChild(headerCell);
+      });
+
+      table.appendChild(headerRow);
+
+      //Create table rows
+      dayObject.hour.forEach((hourObject) => {
+        const hourRow = document.createElement('tr');
+
+        //Hour
+        const hourCell = document.createElement('td');
+        const time = new Date(hourObject.time);
+        hourCell.textContent = time.getHours() + ':00';
+        hourRow.appendChild(hourCell);
+
+        //Temp
+        const tempCell = document.createElement('td');
+        tempCell.classList.add('temp');
+        tempCell.textContent = hourObject.temp_c + '°C';
+        hourRow.appendChild(tempCell);
+
+        //Humidity
+        const humidityCell = document.createElement('td');
+        humidityCell.classList.add('humidity');
+        humidityCell.textContent = hourObject.humidity + '%';
+        hourRow.appendChild(humidityCell);
+
+        //Condition
+        const conditionCell = document.createElement('td');
+        conditionCell.classList.add('condition');
+        conditionCell.textContent = hourObject.condition.text;
+        hourRow.appendChild(conditionCell);
+
+        //Wind
+        const windCell = document.createElement('td');
+        windCell.classList.add('wind');
+        windCell.textContent = hourObject.wind_kph + ' k/h ' + hourObject.wind_dir;
+        hourRow.appendChild(windCell);
+
+        //Rain Chance
+        const rainChanceCell = document.createElement('td');
+        rainChanceCell.classList.add('rain-chance');
+        rainChanceCell.textContent = hourObject.chance_of_rain + '%';
+        hourRow.appendChild(rainChanceCell);
+
+        table.appendChild(hourRow);
+      });
+
+      dayElement.appendChild(table);
+      forecastElement.appendChild(dayElement);
+    });
+
+    containerElement.appendChild(forecastElement);
+  } catch {
+    const errorMessage = "Location's forecast not found. Please check the spelling.";
+    const errorElement = document.createElement('div');
+    errorElement.classList.add('error');
+    errorElement.textContent = errorMessage;
+    containerElement.appendChild(errorElement);
   }
-  const forecastElement = document.createElement('div');
-  forecastElement.className = 'forecast';
-  const weatherObject = await processWeatherData(location);
-  console.log('weatherObject');
-  console.log(weatherObject);
-
-  weatherObject.forecast.forecastday.forEach((dayObject) => {
-    //Create day element and header
-    const dayElement = document.createElement('div');
-    dayElement.classList.add('day');
-    const dateElement = document.createElement('h2');
-    dateElement.classList.add('date');
-    dateElement.textContent = dayObject.date;
-    dayElement.appendChild(dateElement);
-
-    const table = document.createElement('table');
-    table.classList.add('weather-table');
-
-    //Create table headers
-    const headerRow = document.createElement('tr');
-    const headers = ['Hour', 'Temperature', 'Humidity', 'Condition', 'Wind', 'Rain Chance'];
-    const headerClasses = ['hour', 'temp', 'humidity', 'condition', 'wind', 'rain-chance'];
-
-    headers.forEach((headerText, index) => {
-      const headerCell = document.createElement('th');
-      headerCell.textContent = headerText;
-      headerCell.classList.add(headerClasses[index]); // Add class to header cell
-      headerRow.appendChild(headerCell);
-    });
-
-    table.appendChild(headerRow);
-
-    //Create table rows
-    dayObject.hour.forEach((hourObject) => {
-      const hourRow = document.createElement('tr');
-
-      //Hour
-      const hourCell = document.createElement('td');
-      const time = new Date(hourObject.time);
-      hourCell.textContent = time.getHours() + ':00';
-      hourRow.appendChild(hourCell);
-
-      //Temp
-      const tempCell = document.createElement('td');
-      tempCell.classList.add('temp');
-      tempCell.textContent = hourObject.temp_c + '°C';
-      hourRow.appendChild(tempCell);
-
-      //Humidity
-      const humidityCell = document.createElement('td');
-      humidityCell.classList.add('humidity');
-      humidityCell.textContent = hourObject.humidity + '%';
-      hourRow.appendChild(humidityCell);
-
-      //Condition
-      const conditionCell = document.createElement('td');
-      conditionCell.classList.add('condition');
-      conditionCell.textContent = hourObject.condition.text;
-      hourRow.appendChild(conditionCell);
-
-      //Wind
-      const windCell = document.createElement('td');
-      windCell.classList.add('wind');
-      windCell.textContent = hourObject.wind_kph + ' k/h ' + hourObject.wind_dir;
-      hourRow.appendChild(windCell);
-
-      //Rain Chance
-      const rainChanceCell = document.createElement('td');
-      rainChanceCell.classList.add('rain-chance');
-      rainChanceCell.textContent = hourObject.chance_of_rain + '%';
-      hourRow.appendChild(rainChanceCell);
-
-      table.appendChild(hourRow);
-    });
-
-    dayElement.appendChild(table);
-    forecastElement.appendChild(dayElement);
-  });
-
-  containerElement.appendChild(forecastElement);
 }
